3deff8706fe0af2c7c11bad9b37f8498f0fbee6a
Aki <62400162+AkimeAki@users.noreply.github.com>
2023-11-10 22:06:26 +0900

    ボタン追加

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,40 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+interface Props {
+	children: React.ReactNode;
+	selected?: boolean;
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Button = ({ children, selected = false, onClick }: Props): JSX.Element => {
+	return (
+		<button
+			onClick={onClick}
+			css={css`
+				border: none;
+				background-color: ${selected ? "#fd9b6d" : "white"};
+				border-radius: 100px;
+				padding: 5px 20px;
+				cursor: pointer;
+				border: 2px solid white;
+				transition-duration: 200ms;
+				transition-property: background-color;
+
+				@media (prefers-color-scheme: dark) {
+					background-color: ${selected ? "#6ae369" : "white"};
+				}
+
+				&:hover {
+					background-color: ${selected ? "#fd9b6d" : "#ffccb4"};
+
+					@media (prefers-color-scheme: dark) {
+						background-color: ${selected ? "#6ae369" : "#b5ffb4"};
+					}
+				}
+			`}
+		>
+			{children}
+		</button>
+	);
+};
--- src/routes/pages/Portfolio.tsx
+++ src/routes/pages/Portfolio.tsx
@@ -2,8 +2,12 @@
 import { css } from "@emotion/react";
 import { SectionTitle, SectionTitle2 } from "@/components/atoms/SectionTitle";
 import { PortfolioWeb, PortfolioYouTube, PortfolioChromeExtension } from "@/components/atoms/Portfolio";
+import { Button } from "@/components/atoms/Button";
+import { useState } from "react";
 
 export const Portfolio = (): JSX.Element => {
+	const [selectTab, setSelectTab] = useState<"movie" | "web" | "chrome">("movie");
+
 	return (
 		<div
 			css={css`
@@ -19,6 +23,55 @@ export const Portfolio = (): JSX.Element => {
 			<div>
 				<SectionTitle>作ったもの</SectionTitle>
 				<p>作ったもの置いてます。</p>
+			</div>
+			<div
+				css={css`
+					display: flex;
+					flex-direction: column;
+					gap: 20px;
+				`}
+			>
+				<SectionTitle2>カテゴリ</SectionTitle2>
+				<div
+					css={css`
+						display: flex;
+						gap: 20px;
+						flex-wrap: wrap;
+
+						@media screen and (max-width: 900px) {
+							gap: 10px;
+
+							button {
+								font-size: 15px;
+							}
+						}
+					`}
+				>
+					<Button
+						onClick={() => {
+							setSelectTab("movie");
+						}}
+						selected={selectTab === "movie"}
+					>
+						動画
+					</Button>
+					<Button
+						onClick={() => {
+							setSelectTab("web");
+						}}
+						selected={selectTab === "web"}
+					>
+						ウェブサイト
+					</Button>
+					<Button
+						onClick={() => {
+							setSelectTab("chrome");
+						}}
+						selected={selectTab === "chrome"}
+					>
+						Chrome拡張機能
+					</Button>
+				</div>
 			</div>
 			<div
 				css={css`
@@ -40,62 +93,76 @@ export const Portfolio = (): JSX.Element => {
 					}
 				`}
 			>
-				<div>
-					<div>
-						<SectionTitle2>YouTube用エンディング</SectionTitle2>
-						<p>Adobe Premiere ProとAdobe Affter Effectsを使用しました。</p>
-					</div>
-					<PortfolioYouTube url="AzuWH9S4jRk?start=22" />
-				</div>
-				<div>
-					<div>
-						<SectionTitle2>ニコニコ動画用エンディング</SectionTitle2>
-						<p>Adobe Premiere ProとAdobe Affter Effectsを使用しました。</p>
-					</div>
-					<PortfolioYouTube url="9-wqOhxLYyw?start=22" />
-				</div>
-				<div>
-					<div>
-						<SectionTitle2>YouTube用エンディング2</SectionTitle2>
-						<p>AviUtlで作成しました。</p>
-					</div>
-					<PortfolioYouTube url="bxIPbOl98f0" />
-				</div>
-				<div>
-					<div>
-						<SectionTitle2>ポートフォリオ（このサイト）</SectionTitle2>
-						<p>Astro + Reactを使用して作成しました。Cloudflare Pagesでホスティングしています。</p>
-					</div>
-					<PortfolioWeb url="https://aki.wtf/" />
-				</div>
-				<div>
-					<div>
-						<SectionTitle2>ブログ</SectionTitle2>
-						<p>Astroを使用して作成しました。Cloudflare Pagesでホスティングしています。</p>
-					</div>
-					<PortfolioWeb url="https://blog.aki.wtf/" />
-				</div>
-				<div>
-					<div>
-						<SectionTitle2>Keybinds for Google Chat</SectionTitle2>
-						<p>Google ChatのキーバインドをカスタマイズするChrome拡張機能です。</p>
-					</div>
-					<PortfolioChromeExtension id="kabocfciobpmopkcbiphmgdljpdlighk" iframely="RzrynWD" />
-				</div>
-				<div>
-					<div>
-						<SectionTitle2>Convert and Download Image</SectionTitle2>
-						<p>画像の拡張子を変換した上で保存することができるChrome拡張機能です。</p>
-					</div>
-					<PortfolioChromeExtension id="kinldkcfdohpgpedpglhcfjenoaklhkk" iframely="i0SuWW9" />
-				</div>
-				<div>
-					<div>
-						<SectionTitle2>Print for Google Apps Script Page</SectionTitle2>
-						<p>Google Apps Script製のウェブページを正常に印刷できるようにするChrome拡張機能です。</p>
-					</div>
-					<PortfolioChromeExtension id="gacknebdjgldkfjibmbkkdbkihomoiaj" iframely="y7kSzMn" />
-				</div>
+				{selectTab === "movie" && (
+					<>
+						<div>
+							<div>
+								<SectionTitle2>YouTube用エンディング</SectionTitle2>
+								<p>Adobe Premiere ProとAdobe Affter Effectsを使用しました。</p>
+							</div>
+							<PortfolioYouTube url="AzuWH9S4jRk?start=22" />
+						</div>
+						<div>
+							<div>
+								<SectionTitle2>ニコニコ動画用エンディング</SectionTitle2>
+								<p>Adobe Premiere ProとAdobe Affter Effectsを使用しました。</p>
+							</div>
+							<PortfolioYouTube url="9-wqOhxLYyw?start=22" />
+						</div>
+						<div>
+							<div>
+								<SectionTitle2>YouTube用エンディング2</SectionTitle2>
+								<p>AviUtlで作成しました。</p>
+							</div>
+							<PortfolioYouTube url="bxIPbOl98f0" />
+						</div>
+					</>
+				)}
+				{selectTab === "web" && (
+					<>
+						<div>
+							<div>
+								<SectionTitle2>ポートフォリオ（このサイト）</SectionTitle2>
+								<p>Astro + Reactを使用して作成しました。Cloudflare Pagesでホスティングしています。</p>
+							</div>
+							<PortfolioWeb url="https://aki.wtf/" />
+						</div>
+						<div>
+							<div>
+								<SectionTitle2>ブログ</SectionTitle2>
+								<p>Astroを使用して作成しました。Cloudflare Pagesでホスティングしています。</p>
+							</div>
+							<PortfolioWeb url="https://blog.aki.wtf/" />
+						</div>
+					</>
+				)}
+				{selectTab === "chrome" && (
+					<>
+						<div>
+							<div>
+								<SectionTitle2>Keybinds for Google Chat</SectionTitle2>
+								<p>Google ChatのキーバインドをカスタマイズするChrome拡張機能です。</p>
+							</div>
+							<PortfolioChromeExtension id="kabocfciobpmopkcbiphmgdljpdlighk" iframely="RzrynWD" />
+						</div>
+						<div>
+							<div>
+								<SectionTitle2>Convert and Download Image</SectionTitle2>
+								<p>画像の拡張子を変換した上で保存することができるChrome拡張機能です。</p>
+							</div>
+							<PortfolioChromeExtension id="kinldkcfdohpgpedpglhcfjenoaklhkk" iframely="i0SuWW9" />
+						</div>
+						<div>
+							<div>
+								<SectionTitle2>Print for Google Apps Script Page</SectionTitle2>
+								<p>
+									Google Apps Script製のウェブページを正常に印刷できるようにするChrome拡張機能です。
+								</p>
+							</div>
+							<PortfolioChromeExtension id="gacknebdjgldkfjibmbkkdbkihomoiaj" iframely="y7kSzMn" />
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
