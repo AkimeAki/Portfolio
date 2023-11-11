7c751ccc2f8415e341f7e1cac2dbf39cafa76864
Aki <62400162+AkimeAki@users.noreply.github.com>
2023-11-12 01:58:52 +0900

    ロゴ追加

--- src/routes/pages/Works.tsx
+++ src/routes/pages/Works.tsx
@@ -77,11 +77,11 @@ export const Works = (): JSX.Element => {
 				<SectionTitle>金額目安</SectionTitle>
 				<p>あくまで目安なので、金額は上下する可能性があります。</p>
 				<p>また、金額調整、例に書いた要件以外も対応可能なので、気軽にご相談ください。</p>
-				<SectionTitle2>動画編集（例）</SectionTitle2>
+				<SectionTitle2>動画編集</SectionTitle2>
 				<table css={tableStyle}>
 					<thead>
 						<tr>
-							<th>内容</th>
+							<th>内容（例）</th>
 							<th>金額</th>
 						</tr>
 					</thead>
@@ -106,11 +106,11 @@ export const Works = (): JSX.Element => {
 						</tr>
 					</tbody>
 				</table>
-				<SectionTitle2>ウェブ制作（例）</SectionTitle2>
+				<SectionTitle2>ウェブ制作</SectionTitle2>
 				<table css={tableStyle}>
 					<thead>
 						<tr>
-							<th>内容</th>
+							<th>内容（例）</th>
 							<th>金額</th>
 						</tr>
 					</thead>
@@ -133,6 +133,15 @@ export const Works = (): JSX.Element => {
 							<td>20,000円/サイト</td>
 						</tr>
 						<tr>
+							<th>
+								既存サイトのWordPress化
+								<ul>
+									<li>デザインそのまま</li>
+								</ul>
+							</th>
+							<td>30,000円/サイト</td>
+						</tr>
+						<tr>
 							<th>Chrome拡張機能制作</th>
 							<td>2,000円/機能</td>
 						</tr>
@@ -152,7 +161,10 @@ export const Works = (): JSX.Element => {
 				</ul>
 				<SectionTitle2>ウェブ制作</SectionTitle2>
 				<ul>
-					<li>WordPressのテーマ作成</li>
+					<li>
+						<span css={BackImageStyle("/img/icon/white/wordpress.png", 30, -30, -20)}>WordPress</span>
+						のテーマ作成
+					</li>
 					<li>HTML, CSS, JavaScriptなどを用いたコーディング</li>
 					<li>ReactやNextなどを用いたウェブ制作</li>
 					<li>Discord Botの作成</li>
