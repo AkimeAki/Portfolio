2ae9869f6d5a63ab7684ac2b6e1e64a828dea2cd
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-09-02 00:31:33 +0900

    時間表示

--- src/components/os/Taskbar.tsx
+++ src/components/os/Taskbar.tsx
@@ -4,9 +4,30 @@ import { osLoading } from "@/atom";
 import TaskbarIcon from "@/components/os/TaskbarIcon";
 import { css } from "@kuma-ui/core";
 import { useStore } from "@nanostores/react";
+import { useEffect, useState } from "react";
 
 export default function () {
 	const $osLoading = useStore(osLoading);
+	const [nowTime, setNowTime] = useState<string>("");
+
+	useEffect(() => {
+		const id = setInterval(() => {
+			const nowDate = new Date();
+
+			const nowYear = nowDate.getFullYear();
+			const nowMonth = ("00" + (nowDate.getMonth() + 1)).slice(-2);
+			const nowDay = ("00" + nowDate.getDate()).slice(-2);
+			const nowHour = ("00" + nowDate.getHours()).slice(-2);
+			const nowMin = ("00" + nowDate.getMinutes()).slice(-2);
+			const nowSec = ("00" + nowDate.getSeconds()).slice(-2);
+
+			setNowTime(`${nowYear}/${nowMonth}/${nowDay}\n${nowHour}:${nowMin}:${nowSec}`);
+		}, 1000);
+
+		return () => {
+			clearInterval(id);
+		};
+	}, []);
 
 	return (
 		<div
@@ -119,6 +140,25 @@ export default function () {
 						alt="GitHubのアイコン"
 						href="https://github.com/AkimeAki"
 					/>
+				</div>
+				<div
+					className={css`
+						position: absolute;
+						top: 50%;
+						right: 20px;
+						transform: translateY(-50%);
+						color: white;
+						white-space: pre-wrap;
+						font-size: 16px;
+						text-align: right;
+						line-height: 1;
+
+						@media (max-width: 720px) {
+							display: none;
+						}
+					`}
+				>
+					{nowTime}
 				</div>
 			</div>
 		</div>
