be3d93b9fa18a14b3e5ab590ea2ae3405afbcc36
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-09-05 19:30:56 +0900

    明かりチカチカ

--- src/components/os/AkiSignal.tsx
+++ src/components/os/AkiSignal.tsx
@@ -3,9 +3,11 @@
 import { osLoading } from "@/atom";
 import { css } from "@kuma-ui/core";
 import { useStore } from "@nanostores/react";
+import { useState } from "react";
 
 export default function () {
 	const $osLoading = useStore(osLoading);
+	const [animation, setAnimation] = useState<boolean>(true);
 
 	return (
 		<div
@@ -39,8 +41,32 @@ export default function () {
 			/>
 			<img
 				src="/aki-signal.png"
-				alt="でっかい彩季"
-				style={{ animationName: $osLoading ? "" : "aki-signal" }}
+				alt={"でっかい彩季"}
+				style={{ animationName: $osLoading ? "" : animation ? "aki-signal" : "" }}
+				onClick={(e) => {
+					if (animation) {
+						const canvas = document.createElement("canvas");
+						const ctx = canvas.getContext("2d");
+						const image = e.target as HTMLImageElement;
+
+						canvas.width = image.naturalWidth;
+						canvas.height = image.naturalHeight;
+						ctx!.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+						const clickPixelX = e.nativeEvent.offsetX / (image.width / image.naturalWidth);
+						const clickPixelY = e.nativeEvent.offsetY / (image.width / image.naturalHeight);
+
+						const pixelData = ctx!.getImageData(clickPixelX, clickPixelY, 1, 1).data;
+						const alpha = pixelData[3];
+						if (alpha === 255) {
+							canvas.remove();
+							setAnimation(false);
+							setTimeout(() => {
+								setAnimation(true);
+							}, 100);
+						}
+					}
+				}}
 				className={css`
 					position: absolute;
 					bottom: 0;
@@ -52,13 +78,17 @@ export default function () {
 					animation-fill-mode: forwards;
 					animation-iteration-count: 5;
 					animation-timing-function: linear;
+					pointer-events: auto;
+					user-drag: none;
+					-webkit-user-drag: none;
+					-moz-user-select: none;
 					filter: brightness(110%) blur(3px);
 				`}
 			/>
 			<img
 				src="/aki-signal.png"
-				alt="でっかい彩季"
-				style={{ animationName: $osLoading ? "" : "aki-signal" }}
+				alt={"でっかい彩季"}
+				style={{ animationName: $osLoading ? "" : animation ? "aki-signal" : "" }}
 				className={css`
 					position: absolute;
 					bottom: 0;
