5e62611b98cbd6a492a75378d72eeb2e1a20d68b
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-09-07 14:06:53 +0900

    カーソル修正

--- src/components/os/Cursor.tsx
+++ src/components/os/Cursor.tsx
@@ -12,14 +12,8 @@ export default function () {
 	useEffect(() => {
 		let x = 0;
 		let y = 0;
-		let oldX = 0;
-		let oldY = 0;
-		let xDiff: "+" | "-" = "+";
-		let yDiff: "+" | "-" = "+";
 
 		const mousemove = (e: MouseEvent) => {
-			oldX = x;
-			oldY = y;
 			x = e.clientX;
 			y = e.clientY;
 
@@ -49,45 +43,23 @@ export default function () {
 						element.current.style.left = `${x}px`;
 						element.current.style.transitionDuration = "200ms";
 						element.current.style.transitionProperty = "top, left";
-						element.current.style.transitionTimingFunction = "linear";
+						element.current.style.transitionTimingFunction = "ease-out";
 					}
 
 					if (document.body.dataset.tick === "7891011") {
+						element.current.style.top = `${y}px`;
+						element.current.style.left = `${x}px`;
+						element.current.style.transitionDuration = "100ms";
+						element.current.style.transitionProperty = "top, left";
+						element.current.style.transitionTimingFunction = "ease-in-out";
+					}
+
+					if (document.body.dataset.tick === "12") {
+						element.current.style.top = `${y}px`;
+						element.current.style.left = `${x}px`;
 						element.current.style.transitionDuration = "200ms";
 						element.current.style.transitionProperty = "top, left";
-						element.current.style.transitionTimingFunction = "linear";
-
-						if (x > oldX) {
-							element.current.style.left = `${x + 100}px`;
-							xDiff = "+";
-						} else if (x < oldX) {
-							element.current.style.left = `${x - 144}px`;
-							xDiff = "-";
-						} else {
-							if (xDiff === "+") {
-								element.current.style.left = `${x + 100}px`;
-							}
-
-							if (xDiff === "-") {
-								element.current.style.left = `${x - 144}px`;
-							}
-						}
-
-						if (y > oldY) {
-							element.current.style.top = `${y + 100}px`;
-							yDiff = "+";
-						} else if (y < oldY) {
-							element.current.style.top = `${y - 144}px`;
-							yDiff = "-";
-						} else {
-							if (yDiff === "+") {
-								element.current.style.top = `${y + 100}px`;
-							}
-
-							if (yDiff === "-") {
-								element.current.style.top = `${y - 144}px`;
-							}
-						}
+						element.current.style.transitionTimingFunction = "steps(2, end)";
 					}
 				} else {
 					element.current.style.top = `${y}px`;
