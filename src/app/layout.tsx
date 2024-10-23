6283f816313492faca45df64ff1a6694d903393f
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-10-24 02:46:40 +0900

    GTM導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,21 @@ interface Props {
 export default function ({ children }: Props) {
 	return (
 		<html lang="ja">
+			<head>
+				{process.env.NODE_ENV === "production" && process.env.MAINTENANCE === "false" && (
+					<>
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+									new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+									j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+									'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+									})(window,document,'script','dataLayer','GTM-KGV4LZWH');`
+							}}
+						/>
+					</>
+				)}
+			</head>
 			<body data-script="invalid">
 				<script
 					dangerouslySetInnerHTML={{
