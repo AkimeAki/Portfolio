91d13e74157337a883df8dd1a74145b07fcb21b3
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-12-02 07:23:41 +0900

    不要なもの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,6 @@ export default function ({ children }: Props) {
 				<meta name="theme-color" content="#060303" />
 				<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
 				<link rel="sitemap" href="/sitemap.xml" />
-				<script type="text/javascript" src="https://cdn.iframe.ly/embed.js" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700" rel="stylesheet" />
-				<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro" rel="stylesheet" />
 				{process.env.NODE_ENV === "production" && (
 					<>
 						<script
