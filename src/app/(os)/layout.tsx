d2441372c5d987a8404ce721d3ba2cc5d8bccb8f
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-01-22 10:06:39 +0900

    レイアウトの位置を相対から固定に変更し、画面の左上に配置

--- src/app/(os)/layout.tsx
+++ src/app/(os)/layout.tsx
@@ -67,7 +67,9 @@ export default function ({ children }: React.PropsWithChildren) {
 			/>
 			<div
 				className={css`
-					position: relative;
+					position: fixed;
+					top: 0;
+					left: 0;
 					width: 100%;
 					height: 100%;
 				`}
