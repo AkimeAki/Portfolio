b805e78c73d3ec010dd12ce250adfbec5aa77d21
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-04-23 03:11:30 +0900

    OperaのYouTube自動再生禁止

--- src/components/os/background/NewVideo.tsx
+++ src/components/os/background/NewVideo.tsx
@@ -5,16 +5,26 @@ import { css } from "@kuma-ui/core";
 import { useStore } from "@nanostores/react";
 import GlitchWrapper from "@/components/os/GlitchWrapper";
 import { useEffect, useState } from "react";
+import checkBrowser from "@akimeaki/check-browser";
 
 export default function () {
 	const $osReady = useStore(osReady);
 	const [ready, setReady] = useState<boolean>(false);
+	const [browserCheck, setBrowserCheck] = useState<boolean>(false);
 
 	useEffect(() => {
 		if ($osReady) {
 			setReady(true);
 		}
 	}, [$osReady]);
+
+	useEffect(() => {
+		const data = checkBrowser();
+		// AndroidのOperaはYouTubeの自動再生ができない
+		if (!(data.browser === "opera" && data.os === "android")) {
+			setBrowserCheck(true);
+		}
+	}, []);
 
 	return (
 		<GlitchWrapper
@@ -69,20 +79,22 @@ export default function () {
 					background-color: rgb(255, 255, 255, 0.13);
 				`}
 			/>
-			<iframe
-				src="https://www.youtube.com/embed/?list=UUHV3Taosn76pn9_ip1u7Zkg&loop=1&autoplay=1&mute=1&rel=0&controls=0&playsinline=1"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				allowFullScreen
-				className={css`
-					position: absolute;
-					top: 0;
-					left: 0;
-					width: 100%;
-					height: 100%;
-					aspect-ratio: 16/9;
-					border: none;
-				`}
-			/>
+			{browserCheck && (
+				<iframe
+					src="https://www.youtube.com/embed/?list=UUHV3Taosn76pn9_ip1u7Zkg&loop=1&autoplay=1&mute=1&rel=0&controls=0&playsinline=1"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowFullScreen
+					className={css`
+						position: absolute;
+						top: 0;
+						left: 0;
+						width: 100%;
+						height: 100%;
+						aspect-ratio: 16/9;
+						border: none;
+					`}
+				/>
+			)}
 		</GlitchWrapper>
 	);
 }
