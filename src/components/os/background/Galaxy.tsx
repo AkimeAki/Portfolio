788813b67929346149f186bdafd25ed2f98b604e
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-05-13 05:29:28 +0900

    ライト修正

--- src/components/os/background/Galaxy.tsx
+++ src/components/os/background/Galaxy.tsx
@@ -46,8 +46,8 @@ export default function () {
 				const scene = new THREE.Scene();
 
 				// 全方面を照らすライト
-				const ambientLight = new THREE.AmbientLight(undefined, 0.5);
-				scene.add(ambientLight);
+				const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1);
+				scene.add(hemisphereLight);
 
 				// 太陽光
 				const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
