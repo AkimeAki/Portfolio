2fcb0ef43094a1655830b67fa3023b2f78d37674
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-06-01 04:32:19 +0900

    環境整備

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,25 +17,17 @@ export default function ({ children }: Props) {
 			<head>
 				<meta name="theme-color" content="#060303" />
 				<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
-				<link rel="preconnect dns-prefetch" href="//pagead2.googlesyndication.com" crossOrigin="anonymous" />
 				<link rel="sitemap" href="/sitemap.xml" />
 				{process.env.NODE_ENV === "production" && (
-					<>
-						<script
-							dangerouslySetInnerHTML={{
-								__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 									new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 									j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 									'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 									})(window,document,'script','dataLayer','GTM-KGV4LZWH');`
-							}}
-						/>
-						<script
-							async
-							src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6914867149724943"
-							crossOrigin="anonymous"
-						/>
-					</>
+						}}
+					/>
 				)}
 				{process.env.SCAN === "true" && <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />}
 			</head>
