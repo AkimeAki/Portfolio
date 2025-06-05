d91b7c0aa8b497047a86f5dfc89d2de10d159b60
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-06-05 09:37:27 +0900

    モバイル対応

--- src/components/iframe/PortfolioBadge.tsx
+++ src/components/iframe/PortfolioBadge.tsx
@@ -15,9 +15,25 @@ export function PortfolioBadge({ type }: Props) {
 					padding: 7px 11px 10px 12px;
 					font-size: 14px;
 
+					body[data-os="android"] & {
+						padding-bottom: 7px;
+					}
+
+					body[data-browser="safari"] & {
+						padding-bottom: 7px;
+					}
+
 					@media (max-width: 720px) {
 						padding: 4px 9px 8px 10px;
 						font-size: 12px;
+
+						body[data-os="android"] & {
+							padding-bottom: 5px;
+						}
+
+						body[data-browser="safari"] & {
+							padding-bottom: 5px;
+						}
 					}
 				`,
 				type === "work" &&
