a11bb3026d81501251075b1d592ff2fc7ca2583d
AkimeAki <62400162+AkimeAki@users.noreply.github.com>
2025-06-07 23:00:16 +0900

    dynamicParamsテスト

--- tools/clean.ts
+++ tools/clean.ts
@@ -3,6 +3,7 @@ import fs from "node:fs";
 try {
 	fs.rmSync("./.next/", { recursive: true, force: true });
 	fs.rmSync("./.kuma/", { recursive: true, force: true });
+	fs.rmSync("./.open-next/", { recursive: true, force: true });
 	console.log("キャッシュを削除しました。");
 } catch (err) {
 	console.error("キャッシュ削除エラー:", err);
