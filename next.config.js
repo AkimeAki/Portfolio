880101aa94d0305241a694303f155baa770a7beb
AkimeAki <62400162+AkimeAki@users.noreply.github.com>
2025-06-07 22:37:15 +0900

    trailingSlash追加

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ import { withKumaUI } from "@kuma-ui/next-plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	trailingSlash: true,
 	images: {
 		formats: ["image/webp"]
 	}
