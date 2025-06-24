11f4e79c374bfc932290f562a481efdc7ef2a25c
AkimeAki <62400162+AkimeAki@users.noreply.github.com>
2025-06-24 11:43:42 +0900

    コメント追加

--- src/components/URLStateSyncer.tsx
+++ src/components/URLStateSyncer.tsx
@@ -1,30 +1,30 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useWindowManager } from "@/context/WindowManagerContext";
 import { APPS_DATA } from "@/data/app";
 import { pageTitle } from "@/libs/meta";
+import { useUpdateEffect } from "@/hooks/useUpdateEffect";
 
 export function URLStateSyncer() {
 	const { state, dispatch } = useWindowManager();
-	const isFirstRender = useRef(true);
 
-	useEffect(() => {
+	// 開いているアプリとURLを同期
+	useUpdateEffect(() => {
 		const pathSegments = location.pathname.split("/").filter(Boolean);
 		const pathAppId = pathSegments[0];
 
+		// パス変更をするアプリ一覧を取得
 		const changePathApps = [...state.apps].filter(([_id, app]) => {
 			return app.appData.url?.enableChangePath === undefined || app.appData.url.enableChangePath;
 		});
 
-		if (changePathApps.length === 0 && !isFirstRender.current) {
+		// パス変更をするアプリがないときはトップページにする
+		if (changePathApps.length === 0) {
 			window.history.pushState(null, "", "/");
 			document.title = pageTitle;
-			isFirstRender.current = false;
 			return;
 		}
-
-		isFirstRender.current = false;
 
 		const appId = state.sortOrder.at(-1);
 
@@ -47,11 +47,13 @@ export function URLStateSyncer() {
 		}
 	}, [state.sortOrder, state.apps]);
 
+	// 戻る、進むボタンでURLと状態を同期
 	useEffect(() => {
 		function syncUrlToState(e: PopStateEvent) {
 			const pathSegments = location.pathname.split("/").filter(Boolean);
 			const appId = pathSegments[0];
 
+			// ポートフォリオは専用処理があるので除外
 			if (e.state?.app === "portfolio") {
 				return;
 			}
